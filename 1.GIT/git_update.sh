405ef12162541f8773b57438ef3f77f5d5180724
xraz <terintsov95@mail.ru>
2023-06-19 14:27:05 +0300

    Create bash script

--- 1.GIT/git_update.sh
+++ 1.GIT/git_update.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-git fetch origin
-
+git pull origin
+echo 'Удаленный репозиторий загружен'
+date=$(date '+%Y-%m-%d')
+git branch $date
+echo 'Ветка $date создана'
+git checkout $date
+git add .
+git commit -m 'Обновление файлов с локального репозитория:$date'
+git push -u origin $date
+echo 'Скрипт завершил работу'
