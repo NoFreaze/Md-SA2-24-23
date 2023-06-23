89ad0d19d3da40e368e86d60c4e4077b3e8a6315
Илья <62548095+NoFreaze@users.noreply.github.com>
2023-06-23 12:45:44 +0300

    Update check_git_update.sh

--- 1.GIT/BashScripts/check_git_update.sh
+++ 1.GIT/BashScripts/check_git_update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 if [ ! -f log ]; then
     echo "Файл log не найден"
-    git log --pretty=format:"%h - %an, %ar : %s" --graph --since=100.hours --no-merges > log
+    git log --pretty=format:"%h - %an, %ar : %s" --graph --since=1.hours --no-merges > log
     sha256sum log > last_hash
 else
     sha256sum log > last_hash
-    git log --pretty=format:"%h - %an, %ar : %s" --graph --since=100.hours --no-merges > log
+    git log --pretty=format:"%h - %an, %ar : %s" --graph --since=1.hours --no-merges > log
     sha256sum log > new_hash
     if cmp -s last_hash new_hash; then
         echo "Изменений в репозитории не обнаружено"
