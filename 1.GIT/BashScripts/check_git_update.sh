82dea447529c6790134eb237c4d223381f09ad32
Илья <62548095+NoFreaze@users.noreply.github.com>
2023-06-23 12:13:11 +0300

    Update check_git_update.sh

--- 1.GIT/BashScripts/check_git_update.sh
+++ 1.GIT/BashScripts/check_git_update.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 sha256sum log > last_hash
-git log --pretty=format:"%h - %an, %ar : %s" --graph --since=100.hours --no-merges > log
+git log --pretty=format:"%h - %an, %ar : %s" --graph --since=1.hours --no-merges > log
 sha256sum log > new_hash
 if cmp -s last_hash new_hash; then
     echo "Изменений в репозитории не обнаружено"
